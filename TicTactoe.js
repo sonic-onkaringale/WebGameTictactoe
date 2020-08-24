d73601f378fbfd208a944b5014b24561dfc02d14
Onkar Ingale <ingale2273@gmail.com>
2020-08-24 23:06:31 +0530

    Add files via upload

--- TicTactoe.js
+++ TicTactoe.js
@@ -2,9 +2,7 @@
 let currentPlayer = "x";
 
 
-let gameStatus = "Game";
-
-
+let gameStatus = "Game On";
 
 const boxes = document.getElementsByClassName("box");
 if (timeLeft!= 0) {
@@ -16,7 +14,7 @@ for (let i = 0; i < boxes.length; i++) {
     if (boxes[i].innerHTML.trim() == "" && gameStatus == "Game On") {
 
       boxes[i].innerHTML = currentPlayer;
-      //takeMove();
+      takeMove();
 
       currentPlayer = currentPlayer == "x" ? "o" : "x";
 
@@ -92,7 +90,7 @@ for (let i = 0; i < boxes.length; i++) {
 }
 else { for (let i = 0; i < boxes.length; i++) {
     boxes[i].innerHTML = "";
-    boxes[i].style.backgroundColor = "#ae8fb6";
+    boxes[i].style.backgroundColor = "#90CAF9";
     boxes[i].style.color = "black";
   }
   currentPlayer = "x";
@@ -105,7 +103,7 @@ else { for (let i = 0; i < boxes.length; i++) {
 document.getElementById("reset").addEventListener("click", function() {
   for (let i = 0; i < boxes.length; i++) {
     boxes[i].innerHTML = "";
-    boxes[i].style.backgroundColor = "#dee9ec";
+    boxes[i].style.backgroundColor = "#90CAF9";
     boxes[i].style.color = "black";
   }
   currentPlayer = "x";
@@ -115,26 +113,26 @@ document.getElementById("reset").addEventListener("click", function() {
   //  takeMove();
 });
 
-//displays the winner
+
 function showWinner(x, y, z) {
-  boxes[x].style.background = "#ae8fb6";
+  boxes[x].style.background = "#1976D2";
   boxes[x].style.color = "white";
-  boxes[y].style.background = "#ae8fb6";
+  boxes[y].style.background = "#1976D2";
   boxes[y].style.color = "white";
-  boxes[z].style.background = "#ae8fb6";
+  boxes[z].style.background = "#1976D2";
   boxes[z].style.color = "white";
   document.getElementById("winner").innerHTML =
     currentPlayer == "x" ? "O" : "X";
   document.getElementById("message").style.display = "block";
+  document.getElementById("message").style.color="black";
   gameStatus = "Game Over";
- 
+
 }
-//displays the match is draw
+
 function showDraw(){
 	document.getElementById("message").style.display = "block";
 	document.getElementById("winner").innerHTML  = "No";
 	gameStatus = "Game Over";
-	
 }
 var timer;
 var button;
@@ -142,14 +140,12 @@ var timeLeft;
 var label;
 function countdown() {
   if (timeLeft) {
-	  if(gameStatus=="Game Over")
+    //timeLeft = 10;
+    if(gameStatus=="Game Over")
     {
-
       label.innerHTML= "";
       return;
     }
-	 
-	  //timeLeft = 10;
     label.innerHTML = timeLeft;
     timeLeft--;
     timer = setTimeout(countdown, 1000);
@@ -160,36 +156,26 @@ function countdown() {
     timer = undefined;
     
     reset();
-    gameStatus ="Game Over"
+    gameStatus ="Game On"
     
   }
 }
 
 function takeMove() {
-
-  gameStatus = "Game On";
-  if((document.getElementsByClassName("box")[0].innerHTML.length==0) &&
-	   (document.getElementsByClassName("box")[1].innerHTML.length==0) &&
-	   (document.getElementsByClassName("box")[2].innerHTML.length==0) &&
-	   (document.getElementsByClassName("box")[3].innerHTML.length==0) &&
-	   (document.getElementsByClassName("box")[4].innerHTML.length==0) &&
-	   (document.getElementsByClassName("box")[5].innerHTML.length==0) &&
-	   (document.getElementsByClassName("box")[6].innerHTML.length==0) &&
-	   (document.getElementsByClassName("box")[7].innerHTML.length==0) &&
-	   (document.getElementsByClassName("box")[8].innerHTML.length==0) &&
-	   gameStatus == "Game On"){
+  // timer will only be undefined if the game is not started
+  gameStatus = "Game On"
   if (typeof(timer) === "undefined") {
    button.innerHTML = "Reset";
   // document.getElementById("reset").addEventListener("click",function(){
-   timeLeft = 30;
+   timeLeft = 5;
    countdown();
    } else {
  
     clearTimeout(timer);
-    timeLeft = 30;
+    
+    timeLeft = 5;
     
     countdown();
-  }
   }
 }
 
@@ -197,23 +183,22 @@ function reset()
 {
   for (let i = 0; i < boxes.length; i++) {
     boxes[i].innerHTML = "";
-    boxes[i].style.backgroundColor = "#dee9ec";
+    boxes[i].style.backgroundColor = "#90CAF9";
     boxes[i].style.color = "black";
   }
   currentPlayer = "x";
   document.getElementById("message").style.display = "none";
   document.getElementById("player").innerHTML = "X";
-  gameStatus = "Game Over";
+  gameStatus = "Game On";
+  //gameStatus ="Game Over"
   timeLeft =0;
-  label.innerHTML = "" ;
+  label.innerHTML="";
   //countdown();
 }
 
 function init() {
   button = document.getElementById("reset");
-  button2 = document.getElementById("move");
   label = document.getElementById("label");
   button.addEventListener("click", reset);
-  button2.addEventListener("click", takeMove);
 }
 document.addEventListener("DOMContentLoaded", init, false);
